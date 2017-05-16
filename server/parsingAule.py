f98344ec199177a7b59c3099841aa211a8a4aa94
pippomuzzo <pippomuzzo@gmail.com>
2017-05-16 19:11:42 +0200

    Add parsing aule per sedi e attrezzature

---
+++ server/parsingAule.py
@@ -0,0 +1,96 @@
+from xlsschema import SchemaXls
+import numpy as np
+
+class SchemaAule:
+    def __init__(self, file_):
+        self.dati = SchemaXls(file_)
+        self.aule = []
+
+        aule = self.dati.get_rows_list_schema()
+
+        for a in aule:
+            att = {}
+            for k,v in a.items():
+                if k == 'NOME' or k == 'SEDE' or k == 'CAPIENZA':
+                    continue
+                if v.upper() == 'SI':
+                    att[k] = v
+
+            item = Aula(a['NOME'], a['SEDE'], a['CAPIENZA'], att)
+            self.aule.append(item)
+
+
+    def get_lista_aule(self):
+        res = []
+        for a in self.aule:
+            res.append(a.n)
+        return res
+
+    def get_aule_from_index(self, i):
+        for a in self.aule:
+            if a.n == i:
+                return a
+
+    def get_lista_sedi(self):
+        res = np.array([0 for i in xrange(Aula.index)])
+        lista = self.get_lista_aule()
+        for i in lista:
+            a = self.get_aule_from_index(i)
+            res[i] = a.sede.n
+        return res
+
+    def get_matrix_att(self):
+        n_att = len(Aula.map_att)
+        n_aule = Aula.index
+        res = np.array([0 for i in xrange(n_att*n_aule)]).reshape((n_aule, n_att))
+        lista = self.get_lista_aule()
+        for i in lista:
+            a = self.get_aule_from_index(i)
+            l_att = a.list_att
+            for j in l_att:
+                res[i,j] = 1
+        return res
+
+class Aula:
+    map_att = {}
+    index = 0
+
+    def __init__(self, nome, sede, cap, att):
+        self.nome = nome
+        self.cap = cap
+        self.sede = Sede.get_sede(sede)
+        self.sede.add_aula(self)
+        self.att = att
+        self.list_att = self.get_list_att()
+        self.n = Aula.index
+        Aula.index = Aula.index + 1
+
+    def get_list_att(self):
+        res = []
+        for a,v in self.att.items():
+            res.append(Aula.get_n_att(a))
+        return res
+
+    @classmethod
+    def get_n_att(cls, a):
+        if Aula.map_att.has_key(a) == False:
+            Aula.map_att[a] = len(Aula.map_att)
+        return Aula.map_att[a]
+
+
+class Sede:
+    map_sedi = {}
+
+    def __init__(self, nome, n):
+        self.nome = nome
+        self.n = n
+        self.aule = []
+
+    def add_aula(self, aula):
+        self.aule.append(aula)
+
+    @classmethod
+    def get_sede(cls, nome):
+        if Sede.map_sedi.has_key(nome) == False:
+            Sede.map_sedi[nome] = Sede(nome, len(Sede.map_sedi))
+        return Sede.map_sedi[nome]
---
+++ server/xlsschema.py
@@ -0,0 +1,61 @@
+import xlrd
+
+
+class SchemaXls:
+    def __init__(self, file_):
+        workbook = xlrd.open_workbook(file_contents=file_, encoding_override="cp1252")
+        worksheet = workbook.sheet_by_index(0)  # ottengo il primo foglio
+        self.sheet = worksheet
+        self.ncol = self.sheet.ncols
+        self.nrow = self.sheet.nrows
+
+        self.schema = {}
+        for i in xrange(self.ncol):
+            v = self.sheet.cell(0,i).value
+            self.schema[v] = i
+
+    def get_rows_where(self,**kwargs):
+        res = []
+        for i in xrange(1,self.nrow):
+            row = self.sheet.row(i)
+            ok = True
+            for k,v in kwargs:
+                n = self.schema[k]
+                if row[n].value != v:
+                    ok = False
+                    break
+            if ok == True:
+                res.append(row)
+        return res
+
+    def get_first_row_where(self, **kwargs):
+        return self.get_rows_where(kwargs)[0]
+
+    def get_campo(self, row, campo):
+        n = self.schema[campo]
+        return row[n].value
+
+    def get_campo_where(self, campo, **kwargs):
+        row = self.get_first_row_where(kwargs)
+        return self.get_campo(row, campo)
+
+    def get_rows_list(self):
+        res = []
+        for i in xrange(1,self.nrow):
+            r = self.sheet.row(i)
+            res.append(r)
+        return res
+
+    def get_rows_list_schema(self):
+        rows = self.get_rows_list()
+        res = []
+        for r in rows:
+            item = self.get_row_schema(r)
+            res.append(item)
+        return res
+
+    def get_row_schema(self, row):
+        item = {}
+        for k, v in self.schema.items():
+            item[k.upper()] = row[v].value
+        return item
